9cff526bf1d44441217e7626b79f503416b208ca
Christopher Stock <christopher.stock@mayflower.de>
2021-10-08 12:51:37 +0200

    Install npm packages 'chai' and 'supertest'.

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,6 +1,6 @@
 describe( 'Express.js server app', () => {
     it( 'should understand basic mathematical principles', ( done ) => {
-        if (5 === 5) {
+        if ( 5 === 5 ) {
             done();
         } else {
             done( new Error( 'An error occured!' ) );
