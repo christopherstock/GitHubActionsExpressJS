f04f97b1f53156feb091c2fd1779ba6a7bfd457b
Christopher Stock <christopher.stock@mayflower.de>
2021-10-08 14:12:03 +0200

    Complete all express.js tests.

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,51 +1,40 @@
 const request = require( 'supertest' );
 const chai    = require( 'chai'      );
 
-describe( 'Express.js server app', () => {
+describe( 'request Express.js server app', () => {
     let server;
-    beforeEach(function () {
-        server = require('../src/server.js');
-    });
-/*
-    afterEach(function (done) {
-        // server.close( done );
+    beforeEach( () => {
+        server = require( '../src/server' );
+    } );
+    afterEach( ( done ) => {
+        server.close();
         done();
-    });
-*/
-    it('responds to /', function (done) {
-        request(server)
-            .get('/')
-            .expect(200, done);
-    });
+    } );
 
-    it('responds to /user', function (done) {
-        request(server)
-            .get('/user')
-            .set('Content-Type', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, ( err, res ) => {
-                if (err) {
-                    return done(err);
-                }
-                const callStatus = res.body;
+    it( 'respond 200 on base path /', ( done ) => {
+        request( server )
+            .get( '/' )
+            .expect( 200, done );
+    } );
 
-                chai.expect( callStatus ).to.deep.equal(
-                    [
-                        { name: "John Smith",    username: "jsmith"    },
-                        { name: "Jane Williams", username: "jwilliams" },
-                        { name: "Robert Brown",  username: "rbrown"    },
-                    ]
-                );
-
+    it( 'respond 200 and users on path /user', ( done ) => {
+        request( server )
+            .get( '/user' )
+            .set( 'Content-Type', 'application/json' )
+            .expect( 'Content-Type', /json/ )
+            .expect( 200, ( err, response ) => {
+                chai.expect( response.body ).to.deep.equal( [
+                    { name: "John Smith",    username: "jsmith"    },
+                    { name: "Jane Williams", username: "jwilliams" },
+                    { name: "Robert Brown",  username: "rbrown"    },
+                ] );
                 done();
             } );
-
     });
 
-    it('404 everything else', (done) => {
-        console.log('test 404')
-        request(server)
-            .get('/non/existent')
-            .expect(404, done);
-    });
-});
+    it( 'respond 404 on invalid path', ( done ) => {
+        request( server )
+            .get( '/invalid/path' )
+            .expect( 404, done );
+    } );
+} );
