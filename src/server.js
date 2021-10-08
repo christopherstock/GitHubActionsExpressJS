afcc3f1153bc4823a24ea38e3e29fdbd1c743d5f
Christopher Stock <christopher.stock@mayflower.de>
2021-10-08 12:47:06 +0200

    Prune comments from tests. Refactor server creation and startup into new function.

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require( 'express' );
 
-const PORT = 8181;
-const HOST = '0.0.0.0';
+const startServer = ( PORT = 8181, HOST = '0.0.0.0' ) => {
+    const server = express();
 
-// create Express.js app
-const server = express();
+    server.get( '/', ( req, res ) => {
+        res.send( 'Hello World. This is the result from the base route.' );
+    } );
+    server.get( '/user', ( req, res ) => {
+        const users = [
+            { name: "John Smith",    username: "jsmith"    },
+            { name: "Jane Williams", username: "jwilliams" },
+            { name: "Robert Brown",  username: "rbrown"    },
+        ];
+        res.json( users );
+    } );
 
-// specify routes
-server.get( '/', ( req, res ) => {
-    res.send( 'Hello World. This is the result from the base route.' );
-} );
-server.get( '/user', ( req, res ) => {
-    const users = [
-        { name: "John Smith",    username: "jsmith"    },
-        { name: "Jane Williams", username: "jwilliams" },
-        { name: "Robert Brown",  username: "rbrown"    },
-    ];
-    res.json( users );
-} );
+    server.listen( PORT, HOST );
 
-// start Express.js server
-server.listen( PORT, HOST );
+    console.log( `Server running on http://${HOST}:${PORT}` );
 
-console.log( `Running on http://${HOST}:${PORT}` );
+    return server;
+};
+
+startServer();
--- test/serverTest.js
+++ test/serverTest.js
@@ -1,14 +1,8 @@
-// This is just for organisation and reporting
-describe( 'Our application', () => {
-    // This is the name of the test
+describe( 'Express.js server app', () => {
     it( 'should understand basic mathematical principles', ( done ) => {
-
-        // We want this test to pass.
         if (5 === 5) {
-            // If the behavior is as expected, call done with no argument.
             done();
         } else {
-// Otherwise, call done with an error.
             done( new Error( 'An error occured!' ) );
         }
     } );
