873e0b47da6875f8cee117dc14cf9045763cac01
Christopher Stock <christopher.stock@mayflower.de>
2021-10-09 14:55:19 +0200

    Add eslint target to pipeline.

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: 2017
+    },
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
+    },
+    rules: {
+        camelcase: 'warn',
+        complexity: 'off',
+        // enable additional rules
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["warn", "single"],
+        semi: ["error", "always"],
+
+        // override configuration set by extending "eslint:recommended"
+        "no-empty": "warn",
+        "no-cond-assign": ["error", "always"],
+
+        // disable rules from base configurations
+         "for-direction": "off",
+    }
+};
