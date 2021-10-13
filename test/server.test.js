78813fc072beab80852a8adeaeb7168d22abf851
Christopher Stock <christopher.stock@mayflower.de>
2021-10-13 22:10:04 +0200

    Restore test.

--- test/server.test.js
+++ test/server.test.js
@@ -18,6 +18,21 @@ describe('request Express.js server app', () => {
             .expect(200, done);
     });
 
+    it('respond 200 and users on path /user', (done) => {
+        request(server)
+            .get('/user')
+            .set('Content-Type', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, (err, response) => {
+                expect(response.body).toEqual([
+                    {name: 'John Smith',    username: 'jsmith'},
+                    {name: 'Jane Williams', username: 'jwilliams'},
+                    {name: 'Robert Brown',  username: 'rbrown'},
+                ]);
+                done();
+            });
+    });
+
     it('respond 404 on invalid path', (done) => {
         request(server)
             .get('/invalid/path')
